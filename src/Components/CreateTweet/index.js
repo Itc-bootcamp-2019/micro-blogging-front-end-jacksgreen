aaaaa18b87e52896a7cc6e71a92888264c2f2c91
jackgreen <jack_s_green@me.com>
2019-12-16 18:00:14 +0200

    first milestone without local storage

--- src/Components/CreateTweet/index.js
+++ src/Components/CreateTweet/index.js
@@ -3,16 +3,50 @@ import TweetList from '../TweetList'
 import './style.css'
 
 
-function CreateTweet() {
-    return (
-        <div className='create-tweet-wrapper'>
-            <div className='input-box-wrapper'>
-                <textarea className='create-tweet-input-box' placeholder='What do you have in mind?'></textarea>
-                <button>Tweet</button>
+class CreateTweet extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tweetList:[],
+            newTweet: '',
+        }
+    }
+
+
+    onTweetChange(event){
+        this.setState({newTweet: event.target.value})
+    }
+
+    onPublishTweet(){
+        const { newTweet, tweetList, date } = this.state
+        let dateCreated = new Date()
+        dateCreated = dateCreated.toISOString()
+        let tweetObj = {
+            username: 'jack',
+            tweetContent: newTweet,
+            date: dateCreated,
+        }
+        this.setState({
+            tweetList: [tweetObj, ...tweetList ], 
+        })
+    }
+
+    render(){
+        const { tweetList, newTweet } = this.state
+        return (
+            <div className='create-tweet-wrapper'>
+                <div className='input-box-wrapper'>
+                    <textarea onChange ={(event) => this.onTweetChange(event)} className='create-tweet-input-box' placeholder='What do you have in mind?'></textarea>
+                    {newTweet.length <= 140 && <button className="submit-tweet-btn" onClick={() => this.onPublishTweet()}>Tweet</button>}
+                    {newTweet.length > 140 && <button disabled className="submit-tweet-btn disabled-tweet-btn">Tweet</button>}
+                </div>
+                <TweetList tweetList={tweetList}/>
             </div>
-            <TweetList/>
-        </div>
-    )
+        )
+    }
 }
+
+
+
 
 export default CreateTweet
--- src/Components/TweetList/index.js
+++ src/Components/TweetList/index.js
@@ -5,20 +5,21 @@ class TweetList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: 'jack',
-            newTweet: 'On the technical side, Microsoft says the Xbox Series X can handle 4K visuals at 60 frames per second, and potentially up to 120FPS.'
         }
     }
     render() {
-        const { username, newTweet } = this.state;
+        const { tweetList } = this.props
         return (
-            <div className="new-tweet-bg">
-                <div className='tweet-info'>
-                    <div className='tweet-info-text'>{username}</div>
-                    <div className='tweet-info-text'>2019-12-15T14:40:58.340Z</div>
+            tweetList.map(tweet => (
+                <div key={tweet.date} className="new-tweet-bg">
+                    <div className='tweet-info'>
+                        <div className='tweet-info-text'>{tweet.username}</div>
+                        <div className='tweet-info-text'>{tweet.date}</div>
+                    </div>
+                    <div className='new-tweet-content'>{tweet.tweetContent}</div>
                 </div>
-                <div className='new-tweet-content'>{newTweet}</div>
-            </div>
+            ))
+
         )
     }
 }
