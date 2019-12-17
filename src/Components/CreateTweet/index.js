2461cf40d47754421f41c1b254547c49ae63c6ba
jackgreen <jack_s_green@me.com>
2019-12-17 13:52:33 +0200

    milestone 1 complete

--- src/Components/CreateTweet/index.js
+++ src/Components/CreateTweet/index.js
@@ -2,23 +2,22 @@ import React from 'react'
 import TweetList from '../TweetList'
 import './style.css'
 
-
 class CreateTweet extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            tweetList:[],
+            tweetList: [],
             newTweet: '',
         }
     }
 
 
-    onTweetChange(event){
-        this.setState({newTweet: event.target.value})
+    onTweetChange(event) {
+        this.setState({ newTweet: event.target.value })
     }
 
-    onPublishTweet(){
-        const { newTweet, tweetList, date } = this.state
+    onPublishTweet() {
+        const { newTweet, tweetList } = this.state
         let dateCreated = new Date()
         dateCreated = dateCreated.toISOString()
         let tweetObj = {
@@ -26,21 +25,40 @@ class CreateTweet extends React.Component {
             tweetContent: newTweet,
             date: dateCreated,
         }
+        const tweetArray = [tweetObj, ...tweetList];
         this.setState({
-            tweetList: [tweetObj, ...tweetList ], 
+            tweetList: tweetArray
         })
+        this.clearInputBox()
+        localStorage.setItem('tweetList', JSON.stringify(tweetArray))
+        
+    }
+    
+    clearInputBox() {
+        document.getElementById('create-tweet-input-box').value = ''
     }
 
-    render(){
+    componentDidMount(){
+        let savedTweetList = localStorage.getItem('tweetList');
+        savedTweetList = JSON.parse(savedTweetList);
+        this.setState({tweetList: savedTweetList})
+    }
+
+    render() {
         const { tweetList, newTweet } = this.state
+        let disabledButton =
+            <div className="disabled-button-wrapper">
+                <div>The tweet can't contain more then 140 chars.</div>
+                <button disabled className="submit-tweet-btn disabled-tweet-btn">Tweet</button>
+            </div>
         return (
             <div className='create-tweet-wrapper'>
                 <div className='input-box-wrapper'>
-                    <textarea onChange ={(event) => this.onTweetChange(event)} className='create-tweet-input-box' placeholder='What do you have in mind?'></textarea>
+                    <textarea onChange={(event) => this.onTweetChange(event)} id='create-tweet-input-box' placeholder='What do you have in mind?'></textarea>
                     {newTweet.length <= 140 && <button className="submit-tweet-btn" onClick={() => this.onPublishTweet()}>Tweet</button>}
                     {newTweet.length > 140 && <button disabled className="submit-tweet-btn disabled-tweet-btn">Tweet</button>}
                 </div>
-                <TweetList tweetList={tweetList}/>
+                <TweetList tweetList={tweetList} />
             </div>
         )
     }
