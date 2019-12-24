d13a89385866d2e6fba51e22f8d0a3e122c7b0ca
jackgreen <jack_s_green@me.com>
2019-12-24 16:54:50 +0200

    adding firebase login verification

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React from "react";
-import "./App.css";
-import NavBar from "./Components/NavBar";
-import Profile from "./Components/Profile";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomePage from "./Components/HomePage";
-import FirebaseHomePage from "./Components/FirebaseHomePage";
+import React from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Profile from './Components/Profile';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import FirebaseHomePage from './Components/FirebaseHomePage';
+import FirebaseLogin from './Components/FirebaseLogin';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="main-wrapper">
-      <Router>
-        <NavBar />
-        
-        <Switch>
-          <Route exact path="/">
-            {/* <HomePage /> */}
-            <FirebaseHomePage />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignedIn: false
+    };
+  }
+
+  componentDidMount = () => {
+    this.changeLogIn();
+  };
+
+  changeLogIn = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user });
+      console.log('user', user);
+      console.log(this.state.isSignedIn);
+    });
+  };
+
+  render() {
+    return (
+      <div className='main-wrapper'>
+        <Router>
+          <NavBar isSignedIn={this.state.isSignedIn} />
+
+          <Switch>
+            <Route exact path='/'>
+              {/* <HomePage /> */}
+              <FirebaseHomePage isSignedIn={this.state.isSignedIn} />
+            </Route>
+            <Route path='/profile'>
+              <Profile />
+            </Route>
+            <Route path='/login'>
+              <FirebaseLogin
+                isSignedIn={this.state.isSignedIn}
+                changeLogIn={this.changeLogIn}
+              />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./style.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.css';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -10,15 +10,34 @@ class NavBar extends React.Component {
 
   render() {
     return (
-      <div className="navbar-wrapper">
-        <div className="navbar-text">
-          <NavLink to="/" className="nav-text" id="homeButton" activeClassName='selected'>
+      <div className='navbar-wrapper'>
+        <div className='navbar-text'>
+          <NavLink
+            to={this.props.isSignedIn && '/'}
+            className='nav-text'
+            id='homeButton'
+            activeClassName='selected'
+            exact={true}
+          >
             Home
           </NavLink>
-          <NavLink to="/profile" className="nav-text" id="profileButton" activeClassName='selected'>
+          <NavLink
+            to={this.props.isSignedIn && '/profile'}
+            className='nav-text'
+            id='profileButton'
+            activeClassName='selected'
+          >
             Profile
           </NavLink>
         </div>
+        <NavLink
+          to='/login'
+          className='nav-text'
+          id='loginButton'
+          activeClassName='selected'
+        >
+          Login
+        </NavLink>
       </div>
     );
   }
